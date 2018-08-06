5c7c70f3e1fb4240d469c4ab9281bcc39db43b9b
atinfinity <dandelion1124@gmail.com>
2018-08-07 07:04:08 +0900

    +initial code

---
+++ vehicleDetect_svmVar.py
@@ -0,0 +1,13 @@
+# color features
+spatialFeat = True
+spatial_clr = 'YCrCb'
+spatial = 16
+histFeat = True
+histbin = 16
+# hog features
+hogFeat = True
+hog_clrspace = 'YCrCb' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+orient = 9
+pix_per_cell = 8
+cell_per_block = 2
+hog_channel = [0,1,2] # Can be 0, 1, 2
